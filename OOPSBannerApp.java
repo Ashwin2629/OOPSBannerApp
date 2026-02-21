924fe20994c02944b73f353a9e55eb2ff405192c
ASHWIN KARTHIK G <karthikg2629@gmail.com>
2026-02-21 19:57:38 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,11 +1,4 @@
-/**
- * OOPSBannerApp
- * 
- * Use Case 1 (UC1): Print the literal text "OOPS" to console.
- * 
- * @author Ashwin
- * @version 1.0
- */
+
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
